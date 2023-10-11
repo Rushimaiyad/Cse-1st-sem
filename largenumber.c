795b89b25bd3ccbcdc9da557e78f928fffda883f
Rushimaiyad <147418349+Rushimaiyad@users.noreply.github.com>
2023-10-11 09:08:14 +0530

    Add files via upload

---
+++ largenumber.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+void main(){
+	int a,b,c;
+	printf("enter number a");
+	scanf("%d",&a);
+		printf("enter number b");
+	scanf("%d",&b);
+		printf("enter number c");
+	scanf("%d",&c);
+	
+	if(a>b && a>c){
+		printf("a is large");
+	}
+	else if(b>c){
+		printf("b is large");
+	}
+	else{
+		printf("c is large");
+	}
+		
+		
+		
+		
+	
+}
