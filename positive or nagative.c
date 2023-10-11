6c471b60aaa958fc9df4cae083c17a68a147104a
Rushimaiyad <147418349+Rushimaiyad@users.noreply.github.com>
2023-10-11 09:11:44 +0530

    Add files via upload

---
+++ positive or nagative.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+void main(){
+	int n,i,pcount=0,ncount=0;
+	printf("enter a number :");
+	scanf("%d",&n);
+	int arr[i];
+	
+	for(i=0;i<n;i++){
+	
+	printf("enter an element in arr[%d]:", i);
+	scanf("%d",&arr[i]);
+}
+    for(i=0;i<n;i++){
+	
+    if(arr[i]>0){
+    	
+    	pcount++;
+    }
+    else{
+    
+    	ncount++;
+	}
+}
+     printf("positive count %d",pcount);
+     printf("\nnagative count %d",ncount);
+ }
+
+
+
+
