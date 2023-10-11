f5e0906f7c46811002c6a08a929a1d04e09198d4
Rushimaiyad <147418349+Rushimaiyad@users.noreply.github.com>
2023-10-11 09:15:10 +0530

    Add files via upload

---
+++ print in order.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+	int n,i;
+	printf("enter a number :");
+	scanf("%d",&n);
+	int arr[i];
+	
+	for(i=0;i<n;i++){
+	
+	printf("enter an element in arr[%d]:", i);
+	scanf("%d",&arr[i]);
+}
+	for(i=0;i<n;i++){
+		printf("%d",arr[i]);
+	}
+	
+}
---
+++ vowelorconsonuut.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void main(){
+	char a;
+	printf("enter a character");
+	scanf("%c",&a);
+	
+	if(a=='A'||a=='E'||a=='I'||a=='O'||a=='U'){
+		printf("given character is vowel");
+	
+	}else{
+		printf("given character is consonut");
+	}
+		
+	
+	
+	
+}
